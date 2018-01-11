5948e2161f30ec47069da638bc2be921404a6184
odibua <odibua@gmail.com>
2018-01-11 09:13:27 -0800

    comment sobol

--- pceMethod/testPCEMethod.py
+++ pceMethod/testPCEMethod.py
@@ -3,7 +3,13 @@
 """
 Created on Sat Jan  6 21:16:21 2018
 
-@author: odibua
+@author: Ohi Dibua
+This code tests the construction of sobol sensitivity indices using polynomial chaos expansions
+as illustrated in [1]. The functions tested are a basic ishigami function with uniform boundaries
+and a significantly more complicated one with non-uniform boundarie. 
+
+[1] T. Crestaux, O. Maitre, J-M Martinez, "Polynomial Chaoes expansion for sensitivity analysis," 
+    Reliability Engineering and System Safety, 2008.   
 """
 
 import numpy as np
@@ -16,10 +22,21 @@ import sys
 
 testFuncArr = ['ishigamiFundamental','ishigamiComplex']
 #chooseFunc = testFuncArr[1];
-chooseFunc = sys.argv[1];
+#Inputs from terminal that determing the chosen function, the maximum power of an individual
+#polynomial, the level of accuracy of the ingeration, and the dimension, d, of the input.
+chooseFunc = sys.argv[1]; p = int(sys.argv[2]); level=int(sys.argv[3]); d=int(sys.argv[4]);
 
+#Checkts to make sure that inputs into terminal.
 if (chooseFunc not in testFuncArr):
     sys.exit('Input test function does not exist');
+elif (d!=3):
+    sys.exit('Not a valid dimension of input. Choose integer 3');
+elif (level<1):
+    sys.exit('Not a valid level of accuracy. Must be >=1');
+elif (p<(level-1)):
+    sys.exit('Not a valid maximum polynomial power. Select p to be <= level');
+
+#Gives analytical solution to sobol sensitivity indices as comparison to numerical  
 a=7; b=0.1
 if (chooseFunc==testFuncArr[0]):
     funcUse = ishigamiFunctionFundamental;
@@ -33,30 +50,24 @@ if (chooseFunc==testFuncArr[0]):
 elif (chooseFunc==testFuncArr[1]):
     funcUse = ishigamiFunctionComplex;
     sobolAnalyt={};
+    D=420.26428;
     sobolAnalyt['1']=0.02652633381; sobolAnalyt['2']=0.6968539968; sobolAnalyt['3']=0.0098354;
     sobolAnalyt['12']=0.2311640139; sobolAnalyt['13']=0.002524678425; sobolAnalyt['23']=0.02482185803; 
     sobolAnalyt['123']=0.0000827395268;
     
     
-p=18; d = 3; level = p;#int(np.ceil((2*p+1)/2.0));
-
+#p=18; d = 3; level = p;#int(np.ceil((2*p+1)/2.0));
+#Constructs PCE expansion of function of interest, and returns coefficients of expansion
+#the corresponding polynomials, the nodes and weights of the quadrature used to calculate
+#the inner product, a list of powers of inputs that exist in each polynomial, and a list of
+#the inner products of each polynomial. 
 beta,polynomial,nodes,weights,powList,integralArr = genPCEIntegral(p,d,level,customLegendre,funcUse);
 
-nTest=10;
-x = np.zeros((nTest,d)); 
-fAct=np.array([0.0]*nTest); fEst=np.array([0.0]*nTest); err=np.array([0.0]*nTest);
-lb=-1; ub=1;
-for l in range(nTest):
-    fTemp=0;
-    x[l,0:] = (ub-lb)*np.random.random(d) + lb
-    for k in range(len(beta)): 
-        poly=1.0;
-        for j in range(d):
-            poly=poly*customLegendre(powList[k][j],np.array([x[l][j]]));
-        fTemp = fTemp+beta[k]*poly
-    fAct[l]=(funcUse(np.array(x[l,0:])));
-    fEst[l]=fTemp;
-    err[l]=(np.abs(fAct[l]-fEst[l])/fAct[l])
+#Declares class that will be used to calculate sobol indices.
 sobolClass = sobolIndices();
 ###
+#Calculates sobol indices, total sobol indices, and total variance. 
 sobolCache,sobolTotalCache,sTot = sobolClass.genSobolPCE(beta,nodes,weights,powList,customLegendre,integralArr)
+print("\n")
+print('Analytic totral variance',D,'Numeric Total Variance',sTot)
+print('Analytic Sobol Indices',sobolAnalyt,'Numerical Sobol Indices',sobolCache)
